df08c9717edaa5582824876dfa7092cb8d119ec0
Dennis HG <dennishg32@gmail.com>
2021-10-09 19:05:30 +0200

    creation of tasks data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks";
+
 export default {
   name: "App",
   components: {
     Header,
+    Tasks,
   },
   data() {
     return {
@@ -22,6 +26,7 @@ export default {
         id: 1,
         text: "Doctors Appointment",
         day: "March 1st at 2:30pm",
+        reminder: true,
       },
       {
         id: 2,
